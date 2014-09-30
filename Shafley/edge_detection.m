4c9f64c23597028e8b00dee1fa7ac6b5366fc34e
Shafley Xia <shafleyxia@gmail.com>
2014-09-30 11:27:47 +0800

    光栅图

--- Shafley/edge_detection.m
+++ Shafley/edge_detection.m
@@ -1,7 +1,8 @@
 %用Roberts算子，Prewitt算子，Sobel算子，LoG算子，Canny算子进行边缘检测
 
 I=imread('eight.tif');
-% I=imread('psu.jpg');I=rgb2gray(I);
+%I=imread('psu.jpg');I=rgb2gray(I);
+I=imread('deer.jpg');I=rgb2gray(I);
 subplot(231),imshow(I);title('原图');
 B1=edge(I,'roberts',0.04);    %用Roberts算子，梯度阀值为0.04
 B2=edge(I,'prewitt',0.04);    %用Prewitt算子，梯度阀值为0.04
@@ -13,4 +14,5 @@ subplot(233),imshow(B2);title('Prewitt算子');
 subplot(234),imshow(B3);title('Sobel算子');
 subplot(235),imshow(B4);title('LoG算子');
 subplot(236),imshow(B5);title('Canny算子');
+figure,imhist(I);
 
---
+++ Shafley/rgb_grating.m
@@ -0,0 +1,17 @@
+space=32;
+I1 = zeros(512,512);
+I2 = I1;
+I3 = I1;
+J = zeros(512, 512, 3);
+for i=1:512
+  for j=1:512
+    I1(i,j) = 127.5+127.5*cos(j/space*2*pi);
+    I2(i,j) = 127.5+127.5*cos(j/space*2*pi+pi/3);
+    I3(i,j) = 127.5+127.5*cos(j/space*2*pi+2*pi/3);
+  end  
+end
+J(:,:,1) = I1;
+J(:,:,2) = I2;
+J(:,:,3) = I3;
+J = uint8(J);
+figure, imshow(J);
